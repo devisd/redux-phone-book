d4760a4b72f9a2a4941b5713973549ebe582a944
devisd <89788799+devisd@users.noreply.github.com>
2022-08-07 20:47:26 +0700

    add phonebook files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,69 @@
-function App() {
-  return <div>Test Page</div>;
-}
+import { useState, useEffect } from 'react';
+import Form from './components/Form';
+import Contacts from 'components/Contacts';
+import Filter from 'components/Filter';
+import { nanoid } from 'nanoid';
+
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const formSubmitHandler = data => {
+    const newName = contacts.map(el => el.name.toLowerCase());
+    if (newName.includes(data.name.toLowerCase())) {
+      return alert(`${data.name} is already in contacts`);
+    }
+    const input = { id: nanoid(3), name: data.name, number: data.number };
+    setContacts(prevState => [...prevState, input]);
+  };
+
+  const onFilter = event => {
+    setFilter(event.currentTarget.value);
+  };
+
+  const onCheck = () => {
+    if (filter) {
+      return contacts.filter(el =>
+        el.name.toLowerCase().includes(filter.toLowerCase())
+      );
+    } else {
+      return contacts;
+    }
+  };
+
+  const onDeleteContact = id => {
+    const contactName = contacts.filter(el => el.id !== id);
+    setContacts(contactName);
+  };
+
+  const filteredContacts = onCheck();
+
+  return (
+    <div
+      style={{
+        height: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        color: '#010101',
+        padding: 50,
+      }}
+    >
+      <h1>Phonebook</h1>
+      <Form onSubmit={formSubmitHandler} />
+      <Filter filter={filter} onChange={onFilter} />
+      <h2>Contacts</h2>
+      <Contacts contacts={filteredContacts} deleteContact={onDeleteContact} />
+    </div>
+  );
+};
 
 export default App;
